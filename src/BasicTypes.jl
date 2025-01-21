2f5989fdf389c0e9cc94ffdf921a1c1a6f990fcc
Matteo Conti <matteo.conti91@gmail.com>
2025-01-21 13:45:34 +0100

    add initial basic types

--- src/BasicTypes.jl
+++ src/BasicTypes.jl
@@ -1,5 +1,10 @@
 module BasicTypes
 
-# Write your package code here.
+using Unitful: °, rad, Quantity, Length
+using TOML: TOML
+using TerminalLoggers: TerminalLogger, TerminalLoggers
+
+include("basics.jl")
+export ValidAngle, ValidDistance, Constants, NotProvided, NotSimulated, TERMINAL_LOGGER, PKG_VERSION, SCRATCH_DIR, Deg
 
 end
