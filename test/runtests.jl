6cbfd00f544dbe24e252e4b6dbef69947b97850e
Alberto Mengali <disberd@gmail.com>
2025-01-23 21:42:44 +0100

    test both Options

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,16 @@ using TestItemRunner
 end
 
 @testitem "EmptyIterator" begin
-    @test_nowarn for i in NotSimulated()
-        error("ASD")
+
+    for T in (NotSimulated, NotProvided)
+        @test_nowarn for i in T()
+            error("ASD")
+        end
+        @test collect(T()) == Union{}[]
+        @test length(T()) == 0
+        @test eachindex(T()) == Base.OneTo(0)
     end
 
-    @test collect(NotProvided()) == Union{}[]
-
-    @test length(NotSimulated()) == 0
-    @test eachindex(NotSimulated()) == Base.OneTo(0)
 end
 
 @testitem "Terminal Logger" begin
