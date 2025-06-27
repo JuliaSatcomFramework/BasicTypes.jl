93e1729adf3bb7ffad52cc2100a46f0d1fe5b423
Alberto Mengali <disberd@gmail.com>
2025-06-27 13:00:00 +0200

    add deprecation tests

--- test/units.jl
+++ test/units.jl
@@ -24,3 +24,11 @@ end
     @test asdeg(π) ≈ 180.0°
     @test stripdeg(180.0°) ≈ π
 end
+
+@testitem "to_meters/to_length deprecation" begin
+    using BasicTypes: to_meters, to_length
+
+    @test_logs (:warn, r"deprecated") to_meters(10km)
+    @test_logs (:warn, r"deprecated") to_length(u"m", 10km)
+    @test_logs (:warn, r"deprecated") match_mode=:any to_length(u"m")(10km)
+end
